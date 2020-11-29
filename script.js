438ac54b2e548179f2b8e71e47b46f647c4a37c4
itsmattsaudners <itsmattsaunders@gmail.com>
2020-11-30 10:51:18 +1300

    basic video controls completed

---
+++ script.js
@@ -0,0 +1,48 @@
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const stop = document.getElementById('stop');
+const progress = document.getElementById('progress');
+const timestamp = document.getElementById('timestamp');
+
+//Play and Pause Video
+function toggleVideoStatus(){
+  if(video.paused){
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+//Update play/pause icon
+function updatePlayIcon(){
+  if(video.paused) {
+    play.innerHTML = "<i class='fa fa-play fa-2x'></i>";
+  } else {
+    play.innerHTML = "<i class='fa fa-pause fa-2x'></i>";
+  }
+}
+
+//Update progress & timestamp
+function updateProgress(){
+  return true;
+}
+
+//Set video time to progress
+function setVideoProgress(){
+  return true;
+}
+
+//Stop Video
+function stopVideo(){
+  return true;
+}
+
+//Event Listeners
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('timeupdate', updateProgress);
+
+play.addEventListener('click', toggleVideoStatus);
+stop.addEventListener('click', stopVideo);
+progress.addEventListener('change', setVideoProgress)
